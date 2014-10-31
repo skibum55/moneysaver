c9c6d18142dc743adf711815ad021e0bf217ff87
Sean Keery <skibum55@hotmail.com>
2014-10-31 09:58:23 -0600

    Moved was functionality into moneysaver.js

--- moneysaver.js
+++ moneysaver.js
@@ -4,6 +4,26 @@
  * @author Sean Keery <>
  */
 
-function moneysaver() {
-  // TODO: implement your micro framework or library.
-}
+// thanks to http://runnable.com/Um3nhT9LevdQAAC9/stop-an-instance-for-node-js-aws-and-ec2
+var AWS = require('aws-sdk');
+
+// configure AWS security tokens - AWS SDK should use the env setup in CF deploy manifest
+AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID});
+
+// Set your region for future requests.
+AWS.config.update({region: 'us-east-1'});
+
+var ec2 = new AWS.EC2();
+
+console.log(ec2);
+
+ec2.describeRegions({}, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response returns (Array<map>)
+});
+
+ec2.describeInstanceStatus({}, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
